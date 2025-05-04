c4a518ccc9b04ab2654cfddbf324121ea80242d2
jakobschaefer88 <jaschaefer2@mail.fhsu.edu>
2025-05-04 16:58:12 -0500

    fixed weather widget to pull from browser location

--- calendarProject/calendarApp/widgets/weather.py
+++ calendarProject/calendarApp/widgets/weather.py
@@ -1,9 +1,7 @@
 import requests
 import datetime
 from django.conf import settings
-from django.http import request
 from .base import BaseWidget
-from calendarApp.models import UserWidgetConfig
 
 class WeatherWidget(BaseWidget):
     name = 'weather'
@@ -15,45 +13,39 @@ class WeatherWidget(BaseWidget):
     _cached_weather = None
     _cache_date = None
 
-    def get_user_city(self, user):
-        try:
-            config = UserWidgetConfig.objects.get(user=user, widget_name=self.name)
-            return config.config.get('city', 'London')
-        except UserWidgetConfig.DoesNotExist:
-            return 'London'  # fallback
-
-    def get_weather(self, city):
-        today = datetime.date.today()
-        if self._cache_date == today and self._cached_weather and self._cached_weather.get("city") == city:
-            return self._cached_weather
+    def get_weather_by_coords(self, lat, lon):
         try:
             params = {
-                "q": city,
+                "lat": lat,
+                "lon": lon,
                 "appid": self.api_key,
-                "units": "metric",
+                "units": "imperial",
                 "lang": "en",
             }
             response = requests.get(self.url, params=params, timeout=5)
             if response.status_code == 200:
                 data = response.json()
-                weather = {
-                    "city": city,
+                return {
+                    "city": data['name'],
                     "temperature": data['main']['temp'],
                     "description": data['weather'][0]['description'],
                     "humidity": data['main']['humidity'],
                     "wind_speed": data['wind']['speed'],
                     "icon": f"http://openweathermap.org/img/wn/{data['weather'][0]['icon']}@2x.png"
                 }
-                self._cached_weather = weather
-                self._cache_date = today
-                return weather
         except Exception:
             pass
         return {}
 
-    def get_context_data(self, user=None, config=None, request=None):  # added 'request=None'
-        city = self.get_user_city(user)
-        weather_data = self.get_weather(city)
+    def get_context_data(self, user=None, config=None, request=None):
+        lat = request.GET.get("lat")
+        lon = request.GET.get("lon")
+        if lat and lon:
+            weather_data = self.get_weather_by_coords(lat, lon)
+            city = weather_data.get("city", "Unknown")
+        else:
+            weather_data = {}
+            city = "Detecting..."
         return {
             "weather": weather_data,
             "city": city
