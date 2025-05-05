27763a4783397de457fd502c5f7f552610494a18
jakobschaefer88 <jaschaefer2@mail.fhsu.edu>
2025-05-04 21:49:26 -0500

    Update notes calendarProject

--- calendarProject/calendarProject/urls.py
+++ calendarProject/calendarProject/urls.py
@@ -13,7 +13,20 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+INF601 - Programming in Python
+Final Project
+I, Jakob Schaefer, affirm that the work submitted for this assignment is entirely my own.
+I have not engaged in any form of academic dishonesty, including but not limited to cheating, plagiarism, or the use of unauthorized materials.
+I have neither provided nor received unauthorized assistance and have accurately cited all sources in adherence to academic standards.
+I understand that failing to comply with this integrity statement may result in consequences, including disciplinary actions as determined by my course instructor and outlined in institutional policies.
+By signing this statement, I acknowledge my commitment to upholding the principles of academic integrity.
+
+URLS File
+
 """
+
+
 from django.contrib import admin
 from django.urls import path, include
 
